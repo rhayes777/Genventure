9df62be8230a99c8999161bd85c298f018a68c73
Richard <richardhayes777@gmail.com>
2022-11-20 16:35:31 +0000

    better background thresholding

--- image.py
+++ image.py
@@ -83,9 +83,9 @@ class Image:
 
             grey = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
 
-            ret, baseline = cv2.threshold(grey, 127, 255, cv2.THRESH_TRUNC)
+            ret, baseline = cv2.threshold(grey, 201, 255, cv2.THRESH_TRUNC)
 
-            ret, background = cv2.threshold(baseline, 126, 255, cv2.THRESH_BINARY)
+            ret, background = cv2.threshold(baseline, 200, 255, cv2.THRESH_BINARY)
 
             alpha = np.full(src.shape[:2], 255, dtype=np.uint8)
             alpha[background == 255] = 0
@@ -94,27 +94,6 @@ class Image:
             rgba = [b, g, r, alpha]
             dst = cv2.merge(rgba, 4)
             cv2.imwrite(filename, dst)
-
-
-def bgremove2(myimage):
-    # First Convert to Grayscale
-    myimage_grey = cv2.cvtColor(myimage, cv2.COLOR_BGR2GRAY)
-
-    ret, baseline = cv2.threshold(myimage_grey, 127, 255, cv2.THRESH_TRUNC)
-
-    ret, background = cv2.threshold(baseline, 126, 255, cv2.THRESH_BINARY)
-
-    ret, foreground = cv2.threshold(baseline, 126, 255, cv2.THRESH_BINARY_INV)
-
-    foreground = cv2.bitwise_and(myimage, myimage,
-                                 mask=foreground)  # Update foreground with bitwise_and to extract real foreground
-
-    # Convert black and white back into 3 channel greyscale
-    background = cv2.cvtColor(background, cv2.COLOR_GRAY2BGR)
-
-    # Combine the background and foreground to obtain our final image
-    finalimage = background + foreground
-    return finalimage
 
 
 class PlayerImage(Image):
