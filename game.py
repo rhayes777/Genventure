c416cf451548f3de8fd85c463488561937edaa9e
Richard <richardhayes777@gmail.com>
2022-11-20 15:19:40 +0000

    smaller sprite

--- game.py
+++ game.py
@@ -44,8 +44,8 @@ class Game:
         self._running = True
         image = Image(
             prompt="Pixel art of a horse",
-            width=256,
-            height=256,
+            width=32,
+            height=32,
         )
         if not image.exists():
             image.download()
